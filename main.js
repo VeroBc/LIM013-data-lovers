1e8da1e415be27f6db92e2fa3c793eccfe881d6e
Mayra Pérez Aguilar <perezaguilarmayra1997@gmail.com>
2020-09-02 08:33:42 -0500

    main changes

--- main.js
+++ main.js
@@ -1,63 +1,96 @@
 
-// import {filterStatus, filterSpecies, filterGender, order } from './data.js'
+// import { filterStatus, filterSpecies, filterGender, order } from './data.js'
+import data from "./data/rickandmorty/rickandmorty.js";
 
-import data from './data/rickandmorty/rickandmorty.js';
+const allCharacters = data.results;
 
-const list_element = document.getElementById('charactersList');
-const pagination_element = document.getElementById('pagination');
+const listCharactersElement = document.getElementById("listCharacters");
+const paginationElement = document.getElementById('pagination');
 
-let current_page = 1;
+let currentPage = 1;
 let rows = 20;
 
-function DisplayList(items, wrapper, rows_per_page, page) {
+/*----------GENERAL FUNCTION FOR SHOW CHARACTERS----------*/
+function displayList(characters, wrapper, rowsPerPage, page) {
     wrapper.innerHTML = "";
     page--;
 
-    let start = rows_per_page * page;
-    let end = start + rows_per_page;
-    let paginatedItems = items.slice(start, end);
+    let start = rowsPerPage * page;
+    let end = start + rowsPerPage;
+    let paginatedCharacters = characters.slice(start, end);
 
-    for (let i = 0; i < paginatedItems.length; i++) {
-        let item = paginatedItems[i];
+    for (let i = 0; i < paginatedCharacters.length; i++) {
+        let character = paginatedCharacters[i];
 
-        let item_element = document.createElement('div');
-        item_element
-        item_element.classList.add('item');
-        item_element.innerHTML = `<div 
-        data-species="${item.species}"
-        data-gender="${item.gender}"
-        data-status="${item.status}"
+        let characterElement = document.createElement('div');
+        characterElement.classList.add('character');
+        characterElement.innerHTML = `<div class="card-character"
+        data-name="${character.name}"
+        data-status="${character.status}"
+        data-gender="${character.gender}"
+        data-species="${character.species}"
         >
-        <img src="${item.image}" alt="${item.name}">
-        <h2 class="characterName">${item.name}</h2>
+        <img src="${character.image}" alt="${character.name}">
+        <h2>${character.name}</h2>
         </div>`
 
-        wrapper.appendChild(item_element);
+        wrapper.appendChild(characterElement);
+
+        /*----------OVERLAY----------*/
+        const overlay = document.getElementById('overlay');
+        document.querySelectorAll('.listCharacters .character img').forEach((element) => {
+            element.addEventListener('click', () => {
+                const path = element.getAttribute('src');
+                const nameDescription = element.parentNode.dataset.name;
+                const specieDescription = element.parentNode.dataset.species;
+                const genderDescription = element.parentNode.dataset.gender;
+                const statusDescription = element.parentNode.dataset.status;
+
+                overlay.classList.add('active');
+                document.querySelector('#overlay img').src = path;
+                document.querySelector('#overlay .description').innerHTML = `<div>
+                <div class="character-name">${nameDescription}</div>
+                <div>Status: ${statusDescription}</div>
+                <div>Gender: ${genderDescription}</div>
+                <div>Specie: ${specieDescription}</div>
+                </div>`;
+            });
+        });
+
+        document.querySelector('#btn-close').addEventListener('click', () => {
+            overlay.classList.remove('active');
+        });
+
+        overlay.addEventListener('click', (event) => {
+            event.target.id === 'overlay' ? overlay.classList.remove('active') : "";
+        })
     }
 }
-
-function SetupPagination(items, wrapper, rows_per_page) {
+/*----------PAGINATION----------*/
+function setUpPagination(characters, wrapper, rowsPerPage) {
     wrapper.innerHTML = "";
 
-    let page_count = Math.ceil(items.length / rows_per_page);
-    for (let i = 1; i < page_count + 1; i++) {
-        let btn = PaginationButton(i, items);
+    let pageCount = Math.ceil(characters.length / rowsPerPage);
+
+    for (let i = 1; i < pageCount + 1; i++) {
+        let btn = paginationButton(i, characters);
         wrapper.appendChild(btn);
     }
 }
 
-function PaginationButton(page, items) {
+function paginationButton(page, characters) {
     let button = document.createElement('button');
     button.innerText = page;
 
-    if (current_page == page) button.classList.add('active');
-
+    if (currentPage == page) button.classList.add('active');
+    
+    /*------------ACTIVE PAGE BUTTON------------*/
     button.addEventListener('click', function () {
-        current_page = page;
-        DisplayList(items, list_element, rows, current_page);
+        currentPage = page;
+        displayList(characters, listCharactersElement, rows, currentPage);
 
-        let current_btn = document.querySelector('.pagination button.active');
-        current_btn.classList.remove('active');
+        let currentBtn = document.querySelector(".pagination button.active");
+        currentBtn.classList.remove('active');
 
         button.classList.add('active');
     });
@@ -65,32 +98,5 @@ function PaginationButton(page, items) {
     return button;
 }
 
-DisplayList(data.results, list_element, rows, current_page);
-SetupPagination(data.results, pagination_element, rows);
-
-
-
-/*Listener para cada imagen*/
-const overlay = document.getElementById('overlay');
-document.querySelectorAll('.charactersList .item img').forEach((elemento) => {
-
-    elemento.addEventListener('click', () => {
-        const ruta = elemento.getAttribute('src');
-        const descripcion = elemento.parentNode.dataset.species;
-
-        overlay.classList.add('activo');
-        document.querySelector('#overlay img').src = ruta;
-        document.querySelector('#overlay .description').innerHTML = descripcion;
-    });
-});
-
-//Event listener para cerrar
-
-document.querySelector('#btn-cerrar-popup').addEventListener('click', () => {
-    overlay.classList.remove('activo');
-});
-
-//EventListener del overlay
-overlay.addEventListener('click', (evento) => {
-    evento.target.id ===  'overlay' ? overlay.classList.remove('activo') : "";
-})
+displayList(allCharacters, listCharactersElement, rows, currentPage);
+setUpPagination(allCharacters, paginationElement, rows);
