02f953d418a3bdbe73f3222ab64a1cf3918ed330
Vero Bc <verobc.cel@gmail.com>
2020-09-16 19:48:50 -0500

    tests for order function

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,25 +1,66 @@
-import { filterByGender, anotherExample } from './src/data.js';
+import { order, filterByGender, filterByStatus } from '../src/data.js';
+import charactersData from '../src/data/rickandmorty/rickandmorty.js';
+import { outputAscending, outputDescending, outputFemaleFilter, outputAliveFilter } from '../src/data/rickandmorty/__mocks__/rickandmorty.js';
 
 jest.mock("../src/data/rickandmorty/rickandmorty.js");
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
+
+describe('Order: ', () => {
+
+    it('should be an object', () => {
+        expect(typeof order).toBe('object');
+    });
   
-  it.only('returns `example`', () => {
-    console.log(filterByGender("male").length);
-    expect(filterByGender("male")).toBe('example');
-  });
+    describe('order.ascending', () => {
+    
+        it('should be a function', () => {
+            expect(typeof order.ascending).toBe('function');
+        });
+ 
+        it('should return an array in ascending order by name from an array without order', () => {
+            expect(order.ascending(charactersData)).toEqual(outputAscending);
+        });
+    });
+
+    describe('order.descending', () => {
+    
+        it('should be a function', () => {
+            expect(typeof order.descending).toBe('function');
+        });
+ 
+        it('should return an array in descending order by name from an array without order', () => {
+            expect(order.descending(charactersData)).toEqual(outputDescending);
+        });
+    });
+
 });
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
+describe('Filter by Gender: ', () => {
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
+    it('should be a function', () => {
+        expect(typeof filterByGender).toBe('function');
+    });
+
+    it('should return an array only of female characters', () => {
+        expect(filterByGender('female')).toEqual(outputFemaleFilter);
+    });
+
 });
+
+
+describe('Filter by Status: ', () => {
+
+    it('should be a function', () => {
+        expect(typeof filterByStatus).toBe('function');
+    });
+
+    it('should return an array only of alive characters', () => {
+        
+        expect(filterByStatus('alive')).toEqual(outputAliveFilter);
+    });
+
+});
+
+
+
